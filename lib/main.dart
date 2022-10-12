5eb56711569be6bde59c5989045a2b240c8e7b76
YangJianxiang <tospery@gmail.com>
2022-10-12 16:44:56 +0800

    flukit

--- lib/main.dart
+++ lib/main.dart
@@ -1,63 +1,147 @@
-import 'package:flutter/material.dart';
-
-import 'basic/basic_page.dart';
-import 'utils/function.dart';
-import 'widget/widget_page.dart';
+import 'dart:async';
+import 'package:flukit/flukit.dart';
+import 'package:flutter/material.dart' hide Page;
+import 'package:flukit/example/example.dart';
 
 void main() {
-  runApp(const MyApp());
+  final logEmitter = getGlobalLogEmitter();
+  runZoned(
+    () => runApp(const MyApp()),
+    zoneSpecification: ZoneSpecification(
+      print: (Zone self, ZoneDelegate parent, Zone zone, String line) {
+        parent.print(zone, line);
+        // Intercept `print` function and redirect log.
+        logEmitter.value = LogInfo(false, line);
+      },
+      handleUncaughtError: (Zone self, ZoneDelegate parent, Zone zone,
+          Object error, StackTrace stackTrace) {
+        parent.print(zone, '${error.toString()} $stackTrace');
+        // Redirect error log event when error.
+        logEmitter.value = LogInfo(true, error.toString());
+      },
+    ),
+  );
+
+  var onError = FlutterError.onError;
+  FlutterError.onError = (FlutterErrorDetails details) {
+    onError?.call(details);
+    // Redirect error log event when error.
+    logEmitter.value = LogInfo(true, details.toString());
+  };
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Examples',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Examples'),
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      final routesMap = mapRoutes(getRoutes());
+      return MaterialApp(
+        title: 'Flukit',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routes: routesMap,
+        onGenerateRoute: (RouteSettings settings) {
+          String routeName = settings.name!.substring(1).toLowerCase();
+          routeName = Uri.decodeComponent(routeName);
+          return MaterialPageRoute(
+            builder: routesMap[routeName] ??
+                (context) => const MyHomePage(title: 'Flutter Examples'),
+          );
+        },
+        home: const MyHomePage(title: 'Flutter Examples'),
+      );
+    });
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  const MyHomePage({
+    Key? key,
+    required this.title,
+  }) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+//防止热更新失效，我们不定义为静态变量
+List<Page> getRoutes() {
+  return [
+    Page("AfterLayout", const AfterLayoutRoute(), showLog: true),
+    Page(
+      "AccurateSizedBox",
+      const AccurateSizedBoxRoute(),
+      showLog: true,
+    ),
+    Page("AnimatedRotationBox", const AnimatedRotationBoxRoute()),
+    Page("DoneWidget", const DoneWidgetRoute()),
+    Page("GradientButton", const GradientButtonRoute()),
+    Page(
+      "GradientCircularProgressIndicator",
+      const GradientCircularProgressRoute(),
+    ),
+    Page(
+      "KeepAlive Test",
+      const KeepAliveTest(),
+      padding: false,
+      showLog: true,
+    ),
+    Page("LayoutLogPrint", const LayoutLogPrintRoute(), showLog: true),
+    Page("LeftRightBox", const LeftRightBoxRoute()),
+    Page("Log Panel", const LogListenerScopeRoute(), withScaffold: false),
+    Page(
+      "OverflowWithTranslateBox",
+      const OverflowWithTranslateRoute(),
+      padding: false,
+    ),
+    Page("PullRefresh", const PullRefreshRoute(), padding: false),
+
+    Page("Quick Scrollbar", const QuickScrollbarRoute(), padding: false),
+    Page("Swiper", const SwiperRoute()),
+    Page("Swiper Style", const SwiperStyleRoute()),
+    Page("ScaleView", const ScaleViewRoute(), padding: false),
+    Page(
+      "SliverFlexibleHeader",
+      const SliverFlexibleHeaderRoute(),
+      padding: false,
+    ),
+    Page(
+      "SliverHeaderDelegate",
+      const SliverHeaderDelegateRoute(),
+      padding: false,
+    ),
+    Page(
+      "SliverPersistentHeaderToBox",
+      const SliverPersistentHeaderToBoxRoute(),
+      padding: false,
+    ),
+    Page("SlideTransitionX", const SlideTransitionXRoute()),
+    Page("TurnBox", TurnBoxRoute()),
+    Page("WaterMark(水印)", const WatermarkRoute(), padding: false),
+    // Page("AzListView", (ctx) => QuickSelectListViewRoute()),
+  ];
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: ListView(
-        children: generateItem(
-          context,
-          [
-            PageInfo("基础", (ctx) => const BasicPage()),
-            PageInfo("组件", (ctx) => const WidgetPage()),
-            PageInfo("事件", (ctx) => const BasicPage()),
-            PageInfo("通知", (ctx) => const BasicPage()),
-            PageInfo("动画", (ctx) => const BasicPage()),
-            PageInfo("文件操作", (ctx) => const BasicPage()),
-            PageInfo("网络请求", (ctx) => const BasicPage()),
-            PageInfo("本地化", (ctx) => const BasicPage()),
-            PageInfo("高级", (ctx) => const BasicPage()),
-            PageInfo("《Flutter实战（第二版）》", (ctx) => const BasicPage()),
-            // PageInfo("要点", (ctx) => const BasicPage()),
-            // PageInfo("仿XX", (ctx) => const BasicPage()),
-          ],
-        ),
+      appBar: AppBar(title: Text(widget.title)),
+      body: ListPage(
+        children: getRoutes(),
       ),
     );
   }
+}
+
+Map<String, WidgetBuilder> mapRoutes(List<Page> pages) {
+  return pages.fold(<String, WidgetBuilder>{}, (pre, page) {
+    pre[page.title.toLowerCase()] = page.build;
+    return pre;
+  });
 }
