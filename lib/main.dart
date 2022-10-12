a25fbf4cf28209c866ccca4d01afc57504780bfd
YangJianxiang <tospery@gmail.com>
2022-10-12 17:10:07 +0800

    demo

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flukit/flukit.dart';
 import 'package:flutter/material.dart' hide Page;
 import 'package:flukit/example/example.dart';
+import 'inaction/inaction_page.dart';
 
 void main() {
   final logEmitter = getGlobalLogEmitter();
@@ -72,58 +73,59 @@ class MyHomePage extends StatefulWidget {
 //防止热更新失效，我们不定义为静态变量
 List<Page> getRoutes() {
   return [
-    Page("AfterLayout", const AfterLayoutRoute(), showLog: true),
-    Page(
-      "AccurateSizedBox",
-      const AccurateSizedBoxRoute(),
-      showLog: true,
-    ),
-    Page("AnimatedRotationBox", const AnimatedRotationBoxRoute()),
-    Page("DoneWidget", const DoneWidgetRoute()),
-    Page("GradientButton", const GradientButtonRoute()),
-    Page(
-      "GradientCircularProgressIndicator",
-      const GradientCircularProgressRoute(),
-    ),
-    Page(
-      "KeepAlive Test",
-      const KeepAliveTest(),
-      padding: false,
-      showLog: true,
-    ),
-    Page("LayoutLogPrint", const LayoutLogPrintRoute(), showLog: true),
-    Page("LeftRightBox", const LeftRightBoxRoute()),
-    Page("Log Panel", const LogListenerScopeRoute(), withScaffold: false),
-    Page(
-      "OverflowWithTranslateBox",
-      const OverflowWithTranslateRoute(),
-      padding: false,
-    ),
-    Page("PullRefresh", const PullRefreshRoute(), padding: false),
+    Page("Flutter实战（第二版）", const InactionPage()),
+    // Page("AfterLayout", const AfterLayoutRoute(), showLog: true),
+    // Page(
+    //   "AccurateSizedBox",
+    //   const AccurateSizedBoxRoute(),
+    //   showLog: true,
+    // ),
+    // Page("AnimatedRotationBox", const AnimatedRotationBoxRoute()),
+    // Page("DoneWidget", const DoneWidgetRoute()),
+    // Page("GradientButton", const GradientButtonRoute()),
+    // Page(
+    //   "GradientCircularProgressIndicator",
+    //   const GradientCircularProgressRoute(),
+    // ),
+    // Page(
+    //   "KeepAlive Test",
+    //   const KeepAliveTest(),
+    //   padding: false,
+    //   showLog: true,
+    // ),
+    // Page("LayoutLogPrint", const LayoutLogPrintRoute(), showLog: true),
+    // Page("LeftRightBox", const LeftRightBoxRoute()),
+    // Page("Log Panel", const LogListenerScopeRoute(), withScaffold: false),
+    // Page(
+    //   "OverflowWithTranslateBox",
+    //   const OverflowWithTranslateRoute(),
+    //   padding: false,
+    // ),
+    // Page("PullRefresh", const PullRefreshRoute(), padding: false),
 
-    Page("Quick Scrollbar", const QuickScrollbarRoute(), padding: false),
-    Page("Swiper", const SwiperRoute()),
-    Page("Swiper Style", const SwiperStyleRoute()),
-    Page("ScaleView", const ScaleViewRoute(), padding: false),
-    Page(
-      "SliverFlexibleHeader",
-      const SliverFlexibleHeaderRoute(),
-      padding: false,
-    ),
-    Page(
-      "SliverHeaderDelegate",
-      const SliverHeaderDelegateRoute(),
-      padding: false,
-    ),
-    Page(
-      "SliverPersistentHeaderToBox",
-      const SliverPersistentHeaderToBoxRoute(),
-      padding: false,
-    ),
-    Page("SlideTransitionX", const SlideTransitionXRoute()),
-    Page("TurnBox", TurnBoxRoute()),
-    Page("WaterMark(水印)", const WatermarkRoute(), padding: false),
-    // Page("AzListView", (ctx) => QuickSelectListViewRoute()),
+    // Page("Quick Scrollbar", const QuickScrollbarRoute(), padding: false),
+    // Page("Swiper", const SwiperRoute()),
+    // Page("Swiper Style", const SwiperStyleRoute()),
+    // Page("ScaleView", const ScaleViewRoute(), padding: false),
+    // Page(
+    //   "SliverFlexibleHeader",
+    //   const SliverFlexibleHeaderRoute(),
+    //   padding: false,
+    // ),
+    // Page(
+    //   "SliverHeaderDelegate",
+    //   const SliverHeaderDelegateRoute(),
+    //   padding: false,
+    // ),
+    // Page(
+    //   "SliverPersistentHeaderToBox",
+    //   const SliverPersistentHeaderToBoxRoute(),
+    //   padding: false,
+    // ),
+    // Page("SlideTransitionX", const SlideTransitionXRoute()),
+    // Page("TurnBox", TurnBoxRoute()),
+    // Page("WaterMark(水印)", const WatermarkRoute(), padding: false),
+    // // Page("AzListView", (ctx) => QuickSelectListViewRoute()),
   ];
 }
 
